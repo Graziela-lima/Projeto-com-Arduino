f03a78ad8113226dd439f14411c238734cffe4f1
Graziela Lima <grazilima1208@gmail.com>
2026-03-03 23:35:33 -0300

    Update sensor_de_som.ino

--- sensor_de_som.ino
+++ sensor_de_som.ino
@@ -1,20 +1,36 @@
 const int sound = 10;
-const int ledred = 8;
+const int led1 = 13;
+const int led2 = 11;
+const int led3 = 8;
 
+bool ligado = false;
 void setup() {
-  Serial.begin( 9600 );
-  pinMode(ledred, OUTPUT);
-  pinMode(sound, INPUT);
-  digitalWrite(ledred, LOW); 
+Serial.begin( 9600 );
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(sound, INPUT);
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
+digitalWrite(led3, LOW);
 }
 
 void loop() {
-  int valorSound = digitalRead(sound);
-  if (valorSound == LOW) {
-    Serial.println("Som NÃO Detectado");
-    digitalWrite(ledred, LOW);   
- }else{
-    Serial.println("Som Detectado");
-    digitalWrite(ledred, HIGH);  
- }
+int valorSound = digitalRead(sound);
+if (valorSound == HIGH) {
+if(ligado == true){
+Serial.println(&quot;Som não Detectado&quot;);
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
+digitalWrite(led3, LOW);
+ligado = false;
+}else{
+Serial.println(&quot;Som Detectado&quot;);
+digitalWrite(led1, HIGH);
+digitalWrite(led2, HIGH);
+digitalWrite(led3, HIGH);
+ligado = true;
+}
+}
+delay(10);
 }
